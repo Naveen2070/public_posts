c3f82299740da06c0deaaa55bcb1229f90437b1f
Naveen2070 <naveenrameshcud@gmail.com>
2024-06-19 21:09:16 +0530

    working on form actions

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,24 +1,49 @@
+import FormSubmit from '@/components/form-submit';
 import { storePost } from '@/lib/posts';
+import { redirect } from 'next/navigation';
+import { useActionState } from 'react';
 
 export default function NewPostPage() {
   async function createPost(formData) {
-    "use server";
+    'use server';
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
 
-    storePost({
+    let errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push('Title is required');
+    }
+
+    if (!content || content.trim().length === 0) {
+      errors.push('Content is required');
+    }
+
+    if (!image) {
+      errors.push('Image is required');
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+
+    await storePost({
       imageUrl: '',
       title,
       content,
-      userId: 1
-    })
+      userId: 1,
+    });
+
+    redirect('/feed');
   }
+
+  const [state, formAction] = useActionState(createPost, {});
 
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
@@ -37,8 +62,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
