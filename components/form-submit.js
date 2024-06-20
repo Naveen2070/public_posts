bdadd2830d35bb306597ac8d2d328e99bc7542a6
Naveen2070 <104181086+Naveen2070@users.noreply.github.com>
2024-06-20 17:09:33 +0530

    completed

--- components/form-submit.js
+++ components/form-submit.js
@@ -1,11 +1,12 @@
-'use client';
+"use client";
+
 import { useFormStatus } from 'react-dom';
 
 export default function FormSubmit() {
   const status = useFormStatus();
 
   if (status.pending) {
-    return <p>Creating post.....</p>;
+    return <p>Creating post...</p>;
   }
 
   return (
