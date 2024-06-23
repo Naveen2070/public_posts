e716362ad5386bc3254816302aa44fd2dc2324cc
Naveen2070 <naveenrameshcud@gmail.com>
2024-06-23 17:53:08 +0530

    added section app optimizations

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,15 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export async function generateMetadata() {
+//   const posts = await getPosts();
+//   const count = posts.length;
+//   return {
+//     title: `Posts (${count})`,
+//     description: 'This is the feeds section',
+//   };
+// }
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
